cc6a4d79673f5ec55111d90092950f078c8640b0
ItsDarker <finofranklin@gmail.com>
2023-02-05 11:43:58 +0530

    hello

--- pages/index.js
+++ pages/index.js
@@ -251,18 +251,6 @@ const Home = () => {
           <button className="enter-button" onClick={handleEnter}>Enter</button>
         </div>
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
-      </div>
     </div>
   );
 };
